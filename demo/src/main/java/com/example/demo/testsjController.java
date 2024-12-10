f1d7a0b6a92f86d8f3909cd285d0d2918ad6a6af
kimggor <dkdjsema9808@naver.com>
2024-12-10 10:39:39 +0900

    dev/feature/testsj 커밋

--- demo/src/main/java/com/example/demo/testsjController.java
+++ demo/src/main/java/com/example/demo/testsjController.java
@@ -2,6 +2,6 @@ package com.example.demo;
 
 public class testsjController {
     public static void main(String[] args) {
-        System.out.println("세중");
+        System.out.println("세중 dev/feature/testsj");
     }
 }
