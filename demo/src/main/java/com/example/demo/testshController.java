69554e42be10291e24bc003f273ee043aa717b75
Ekoro12 <zxcvb1234465@NAVER.COM>
2024-12-10 10:57:06 +0900

    112

--- demo/src/main/java/com/example/demo/testshController.java
+++ demo/src/main/java/com/example/demo/testshController.java
@@ -3,5 +3,7 @@ package com.example.demo;
 public class testshController {
     public static void main(String[] args) {
         System.out.println("세현");
+        System.out.println("세현");
+        System.out.println("세현");
     }
 }
